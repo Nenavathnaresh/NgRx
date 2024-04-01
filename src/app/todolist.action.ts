b95ddb3f7ce2cb9e7dc946d0e992f11385a1bad0
Nenavath Naresh <nenavathnaresh460@gmail.com>
2024-04-01 22:16:45 +0530

    bn

--- src/app/todolist.action.ts
+++ src/app/todolist.action.ts
@@ -4,4 +4,4 @@ export const addtodo = createAction("[Todolist Component] Addtodo", props<any>()
 export const deleteTodo = createAction("[Todolist Component] Delete", props<any>())
 export const doneTodo = createAction("[Todolist Component] Done", props<any>())
 export const undoTodo = createAction("[Todolist Component] Undo", props<any>())
-// console.log(props);
+// console.log((props<any>()));
