6075937fc307a708e9e537a9ac16da35d31f1f71
Nenavath Naresh <nenavathnaresh460@gmail.com>
2024-04-04 18:01:34 +0530

    done with radio, checkbox and select boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx';
+  gen:string = ''
+  techs:string[] = [];
+  selectedCountry:string = ''
+
+  update(e:any){
+    if(e.target.checked){
+      this.techs.push(e.target.value)
+    }
+    else{
+      this.techs = this.techs.filter((tech)=>{
+        if(e.target.value !== tech){
+          return true
+        }
+        else{
+          return false
+        }
+      })
+    }
+    console.log(this.techs);
+    
+  }
 }
